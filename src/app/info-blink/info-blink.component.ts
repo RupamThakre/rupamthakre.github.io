a9c6b3198ef355f7f055bdac38c010cf95d7a09a
Rupam Thakre <rupamthakre15@gmail.com>
2023-03-07 12:14:41 +0530

    code updated2

---
+++ src/app/info-blink/info-blink.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-infoblink',
+  templateUrl: './info-blink.component.html'
+})
+export class InfoBlinkComponent {
+  title = 'myApp';
+ 
+
+}
