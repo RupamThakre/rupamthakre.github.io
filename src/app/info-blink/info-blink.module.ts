c521d2ae1c55bbdb5aca8485568d4161964a1ee5
Rupam Thakre <rupamthakre15@gmail.com>
2023-03-07 10:50:19 +0530

    app code added1

---
+++ src/app/info-blink/info-blink.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { ToastrModule } from 'ngx-toastr';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { NgxPaginationModule } from 'ngx-pagination';
+import { InfoBlinkRoutingModule } from './info-blink-routing.module';
+
+@NgModule({
+  declarations: [
+    AddStudentComponent,
+    EditStudentComponent,
+    StudentListComponent
+  ],
+  imports: [
+    CommonModule,
+    ToastrModule.forRoot(), // ToastrModule added,
+    ReactiveFormsModule,
+    FormsModule,
+    NgxPaginationModule,
+    InfoBlinkRoutingModule
+  ]
+})
+export class InfoBlinkModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+      apiKey: "AIzaSyDQniFv0MVk-P6av15M29jGCCthbXwk_DE",
+      authDomain: "infoblink-8a750.firebaseapp.com",
+      databaseURL: "https://infoblink-8a750-default-rtdb.firebaseio.com",
+      projectId: "infoblink-8a750",
+      storageBucket: "infoblink-8a750.appspot.com",
+      messagingSenderId: "847119905541",
+      appId: "1:847119905541:web:50896fbc5d85a5338e3d6c",  
+      measurementId: "G-MD5VB7MNDS"
+    }
+  };
